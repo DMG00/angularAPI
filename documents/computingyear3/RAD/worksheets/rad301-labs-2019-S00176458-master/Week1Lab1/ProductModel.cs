a94d3f2e72a40b1a1417d077be2f77acf673f5ef
dillonmcgroary00 <dillonmcgroary00@gmail.com>
2019-10-22 14:35:58 +0100

    Week1Lab1 Part 2 Complete

---
+++ documents/computingyear3/RAD/worksheets/rad301-labs-2019-S00176458-master/Week1Lab1/ProductModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Lab1
+{
+    public class ProductModel
+    {
+        public List<Product> Products = new List<Product>()
+        {
+            new Product{ProductID = 1, Description = "9inch nails", QTY = 200, UnitPrice= 0.10f, CategoryID=1},
+            new Product{ProductID = 2, Description = "9inch bolts", QTY = 120, UnitPrice= 0.20f, CategoryID=1},
+            new Product{ProductID = 3, Description = "Chimney Hoover", QTY = 10, UnitPrice= 100.50f, CategoryID=2},
+            new Product{ProductID = 4, Description = "Washing Machine", QTY = 7, UnitPrice= 399.99f, CategoryID=2}
+        };
+
+        public List<Supplier> Suppliers = new List<Supplier>()
+        {
+            new Supplier{SupplierID = 1, SupplierName = "Supplier 1", AddressLine1 = "Addr 11", AddressLine2 = "Addr 21"},
+            new Supplier{SupplierID = 1, SupplierName = "Supplier 2", AddressLine1 = "Addr 11", AddressLine2 = "Addr 21"}
+        };
+
+        public List<Category> CategoryList = new List<Category>()
+        {
+            new Category{CategoryID = 1, Description = "Hardware"},
+            new Category{CategoryID = 2, Description = "Electrical Appliances"}
+        };
+
+        public List<SupplierProduct> SuppliersProducts = new List<SupplierProduct>()
+        {
+            new SupplierProduct{SupplierID = 1, ProductID = 1, DateFirstSupplied = new DateTime(2012,12,12)},
+            new SupplierProduct{SupplierID = 1, ProductID = 2, DateFirstSupplied = new DateTime(2013,01,06)},
+            new SupplierProduct{SupplierID = 2, ProductID = 3, DateFirstSupplied = new DateTime(2017,09,09)},
+            new SupplierProduct{SupplierID = 2, ProductID = 4, DateFirstSupplied = new DateTime(2017,09,10)}
+        };
+    }
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public int QTY { get; set; }
+        public float UnitPrice { get; set; }
+        public int CategoryID { get; set; }
+
+
+        public override string ToString()
+        {
+            return String.Concat(new string[]
+            { " ProductID:", ProductID.ToString(),
+                   " Description ",Description,
+                   " Quantity: ", QTY.ToString(),
+                   " Unit Price: ", UnitPrice.ToString(),
+                   " CategoryID: ", CategoryID.ToString()});
+        }
+    }
+
+    public class Supplier
+    {
+        public int SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+
+        public override string ToString()
+        {
+            return string.Concat(new string[] {
+                "SupplierID: ", SupplierID.ToString(),
+                "Supplier Name: ", SupplierName,
+                "Address Line 1: ", AddressLine1,
+                "Address Line 2: ", AddressLine2 });
+        }
+    }
+
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return string.Concat(new string[] {
+                "CategoryID: ", CategoryID.ToString(),
+                "Description: ", Description });
+        }
+    }
+
+    public class SupplierProduct
+    {
+        public int SupplierID { get; set; }
+        public int ProductID { get; set; }
+        public DateTime DateFirstSupplied { get; set; }
+
+        public override string ToString()
+        {
+            return string.Concat(new string[] {
+                "SupplierID: ",SupplierID.ToString(),
+                "ProductID: ", ProductID.ToString(),
+                "Date First Supplied: ", DateFirstSupplied.ToString() });
+        }
+    }
+}
---
+++ documents/computingyear3/RAD/worksheets/rad301-labs-2019-S00176458-master/Week1Lab1/Program.cs
@@ -0,0 +1,97 @@
+﻿using ActivityTracker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity.Track("Designing Classes Model");
+            Activity.Track("Starting Queries");
+
+            ProductModel pm = new ProductModel();
+
+            //List Categories
+            foreach (var item in pm.CategoryList)
+            {
+                Console.WriteLine("Category: {0}", item.ToString());
+            }
+
+            //List products
+            foreach (var item in pm.Products)
+            {
+                Console.WriteLine("Product: {0}", item.ToString());
+            }
+
+            //Quantity is less than 100
+            var productQuantity = (from p in pm.Products
+                                   where p.QTY <= 100
+                                   select p);
+
+            foreach (var item in productQuantity)
+            {
+                Console.WriteLine("Products with quantity less than 100: {0}", item.ToString());
+            }
+
+            var ProductsWithCost = pm.Products.Select(p => new { p.ProductID, p.Description, p.QTY, p.UnitPrice, TotalCost = p.UnitPrice * p.QTY });
+            Console.WriteLine();
+            foreach (var item in ProductsWithCost)
+            {
+                Console.WriteLine(String.Format("|{0,5}|{1,5}|{2,5}|{3,5:c}|{4,5:c}|", item.ProductID, item.Description, item.QTY, item.UnitPrice, item.TotalCost));
+            }
+            Console.WriteLine();
+
+
+
+            //Products from the hardware category
+            var HardwareProducts = (from p in pm.Products
+                                    join c in pm.CategoryList
+                                    on p.CategoryID equals c.CategoryID
+                                    where c.Description == "Hardware"
+                                    select p);
+
+            foreach (var item in HardwareProducts)
+            {
+                if (item.CategoryID == 1)
+                {
+                    Console.WriteLine("Products in hardware: {0}", item.ToString());
+                };
+            }
+
+            //list all suppliers and their parts ordered by supplier name
+            var SupplierProductsGrouped = (from s in pm.Suppliers
+                                           join sp in pm.SuppliersProducts
+                                           on s.SupplierID equals sp.SupplierID
+                                           join p in pm.Products
+                                           on sp.ProductID equals p.ProductID
+                                           select new
+                                           {
+                                               s.SupplierName,
+                                               Products = p
+                                           }
+                         ).OrderBy(o => o.SupplierName)
+                         .GroupBy(g => g.SupplierName);
+
+            // Group has a Key field and a set of elements stored under that key field
+            foreach (var group in SupplierProductsGrouped)
+            {
+                Console.WriteLine($"Supplier Name: {group.Key}");
+
+                for (int i = 0; i < group.Count(); i++)
+                {
+                    Console.WriteLine("Product {0}", group.ElementAt(i).Products.ToString());
+                }
+            }
+
+
+            Console.ReadKey();
+
+            Activity.Track("Finished Queries");
+        }
+    }
+}
